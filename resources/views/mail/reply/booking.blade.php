ef3cdf6d5be23fe9069025c5d79603e61f24e56f
Ayako Towatari <kyotayako@gmail.com>
2020-11-05 13:34:44 +0900

    email template changed.

--- resources/views/mail/reply/booking.blade.php
+++ resources/views/mail/reply/booking.blade.php
@@ -9,8 +9,13 @@ We are working hard to get ready for the launch of the preview, and we will get 
 
 Meanwhile, please do not hesitate to let us know if you have any questions. We very much look forward to working with you soon.
 
-Kindest regards,<br>
-Ayako Towatari
+Kind Regards,<br>
+Ayako Towatari<br>
 
-{{ config('app.name') }}
+meetUni<br>
+4-2-13-705 Aobadai, Meguro-Ku, Tokyo,153-0042, Japan<br>
+mobile: +81 80 6571 5106<br>
+email: ayako.towatari@meetuniglobal.com<br>
+URL: https://invitation.meetuniglobal.com<br>
+
 @endcomponent
--- resources/views/mail/reply/contact.blade.php
+++ resources/views/mail/reply/contact.blade.php
@@ -10,7 +10,12 @@ Content:
 {{ $user->content }}
 
 Kind Regards,<br>
-Ayako Towatari
+Ayako Towatari<br>
 
-{{ config('app.name') }}
+meetUni<br>
+4-2-13-705 Aobadai, Meguro-Ku, Tokyo,153-0042, Japan<br>
+mobile: +81 80 6571 5106<br>
+email: ayako.towatari@meetuniglobal.com<br>
+URL: https://invitation.meetuniglobal.com<br>
+
 @endcomponent
