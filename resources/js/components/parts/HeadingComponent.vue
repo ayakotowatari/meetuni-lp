e31e5d8d15f2211122a2da1e9c724ed49e874b60
Ayako Towatari <kyotayako@gmail.com>
2020-12-18 13:52:10 +0900

    Minor changes

--- resources/js/components/parts/HeadingComponent.vue
+++ resources/js/components/parts/HeadingComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1 class="primary--text mb-4 heading">meetUni</h1>
-        <h2 class="grey--text text--darken-1 subheading">Made for reaching out to prospective international students <br class="hidden-sm-and-down">more easily and simply</h2>
+        <h2 class="grey--text text--darken-1 subheading">Made for reaching out to prospective international students <br class="hidden-sm-and-down">more easily and flexibly</h2>
     </div>
 </template>
 
